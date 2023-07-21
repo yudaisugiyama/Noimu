710c4cd7fab672433bcd465560db7bcce1aea63e
yudai <sugi.nut@gmail.com>
2023-07-22 00:13:15 +0900

    杉山テスト用スクリプト追加

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
+"""杉山用テスト用スクリプト
+
+"""
+
 from typing import *
 
 from communication_thread import Communicator
@@ -6,27 +10,21 @@ from config import feedback_queue
 
 
 def initialize() -> Communicator:
-    """強化学習モデル生成 + 通信機の生成 + 各スレッドの生成
-
-    Returns:
-        Tuple[DQN, Communicator]: モデルスレッドとコミュニケーションスレッド
-    """
-
-    # 強化学習モデルとスレッド生成
-    # open_calm = OpenCALM()
+    # LLMモデルとスレッド生成
+    open_calm = OpenCALM()
 
     # 通信用スレッド生成
     communicator = Communicator()  # Unityとの通信機
 
-    return communicator
+    return open_calm, communicator
 
 
 def main():
-    communicator = initialize()
+    open_calm, communicator = initialize()
 
     # スレッドの開始
     communicator.start()
-    # open_calm.start()
+    open_calm.start()
 
 
 if __name__ == "__main__":
