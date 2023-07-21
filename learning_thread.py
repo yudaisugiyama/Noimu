8098812befc1124ba503aeff21fd787e3d22f65f
CURRY-AND-RICE <progoogram@gmail.com>
2023-07-21 23:44:50 +0900

    ひな形を追加

--- learning_thread.py
+++ learning_thread.py
@@ -6,3 +6,16 @@
 
 - mqttから受け取った起きるまでの時間がFEEDBACK_TIME_QUEUEに入ってくるので、入り次第学習する
 """
+
+import threading
+from soundAI.rl_model import RLModel
+
+
+class DQN(threading.Thread):
+    def __init__(self, rl_model: RLModel) -> None:
+        super().__init__()
+        self.rl_model = rl_model
+        raise NotImplementedError
+
+    def run(self) -> None:
+        raise NotImplementedError
---
+++ soundAI/rl_model.py
@@ -0,0 +1,3 @@
+class RLModel:
+    def __init__(self) -> None:
+        raise NotImplementedError
