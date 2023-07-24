2e6ee403ac2ee8b259a50cd3d4546a98fa1773fe
yudai <sugi.nut@gmail.com>
2023-07-24 23:22:17 +0900

    #6 フォルダ名修正、adapter_config追加

--- open_calm_thread.py
+++ open_calm_thread.py
@@ -12,6 +12,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import threading
 from geopy.geocoders import Nominatim
 
+import torch
+from peft import PeftModel, PeftConfig
+import scipy
+
 
 class OpenCALM(threading.Thread):
     def __init__(self, prompt_queue, location):
@@ -24,8 +28,51 @@ class OpenCALM(threading.Thread):
         self.prompt_queue = prompt_queue
         self.location = location
 
+        # 基本パラメータ
+        model_name = "cyberagent/open-calm-small"
+        # dataset = "kunishou/databricks-dolly-15k-ja"
+        peft_name = "./lora-calm-small"
+        # output_dir = "lora-calm-small-results"
+
+        # モデルの準備
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_name,
+            load_in_8bit=True,
+            device_map="auto",
+        )
+
+        # トークンナイザーの準備
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+        # LoRAモデルの準備
+        self.model = PeftModel.from_pretrained(
+            self.model,
+            peft_name,
+            device_map="auto"
+        )
+
+        # 評価モード
+        self.model.eval()
+        self.generate("おはよ〜！今日の長岡技術科学大学の空は晴れだから、")
+
         # 天気情報取得
         # self.get_weather()
+    
+    # テキスト生成関数の定義
+    def generate(self, prompt, maxTokens=128):
+        # 推論
+        input_ids = self.tokenizer(prompt, return_tensors="pt", truncation=True).to(self.device)
+        outputs = self.model.generate(
+            input_ids=input_ids,
+            max_new_tokens=maxTokens,
+            do_sample=True,
+            temperature=0.7,
+            top_p=0.75,
+            top_k=40,
+            no_repeat_ngram_size=2,
+        )
+        outputs = outputs[0].tolist()
+        print(self.tokenizer.decode(outputs))
 
 
     def run(self):
@@ -66,7 +113,7 @@ class OpenCALM(threading.Thread):
 
         return str(lat), str(lon)
     
-    def get_weather(self) -> str:
+    def get_weather(self):
         # 天気API使用
         load_dotenv()
         self.api_key = os.getenv("API_TOKEN_OPEN_WEATHER_MAP")
