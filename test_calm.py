9d7fa23e88a8d0958e4b7c99c88af03aaa591469
yudai <sugi.nut@gmail.com>
2023-07-26 01:25:24 +0900

    fix #6 ファインチューニングモデル実装

--- test_calm.py
+++ test_calm.py
@@ -1,5 +1,5 @@
 import torch
-from peft import PeftModel, PeftConfig
+from peft import PeftModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # 基本パラメータ
@@ -7,31 +7,52 @@ model_name = "cyberagent/open-calm-small"
 peft_name = "lora-calm-small"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# モデルの準備
 model = AutoModelForCausalLM.from_pretrained(
-    "cyberagent/open-calm-small", device_map="auto", torch_dtype=torch.float32
+    model_name,
+    torch_dtype=torch.float32
 )
+model.to(device)
 
 # トークンナイザーの準備
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+
 # LoRAモデルの準備
 model = PeftModel.from_pretrained(
-    model, 
-    peft_name, 
+    model,
+    peft_name,
     device_map="auto"
 )
 
 # 評価モード
 model.eval()
 
+# プロンプトテンプレートの準備
+def generate_prompt(data_point):
+    if data_point["input"]:
+        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Input:
+{data_point["input"]}
+
+### Response:"""
+    else:
+        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{data_point["instruction"]}
+
+### Response:"""
+
 # テキスト生成関数の定義
 def generate(prompt, maxTokens=128):
     # 推論
-    input_ids = tokenizer(prompt, return_tensors="pt", truncation=True)
-
-    # モデルのgenerateメソッドをCPUで実行するために、input_idsをCPUに移動
-    input_ids = input_ids.to(torch.device('cpu'))
-
+    input_ids = tokenizer(prompt, return_tensors="pt", truncation=True).input_ids.to(device)
     outputs = model.generate(
         input_ids=input_ids,
         max_new_tokens=maxTokens,
